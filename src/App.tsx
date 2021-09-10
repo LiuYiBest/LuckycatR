af26445341d99685dbd934489c4d7269b383e1ff
chunfen <1070692044@qq.com>
2021-09-10 10:53:05 +0800

    划分导航页面组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,32 @@
 import React from 'react';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-
-import Layout from 'components/Layout';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 
 function App() {
-  return (
-      <Router>
-        <Switch>
-          <Route path="/tags">
-            <Tags/>
-          </Route>
-          <Route path="/money">
-            <Money/>
-          </Route>
-          <Route path="/statistics">
-            <Statistics/>
-          </Route>
-          <Redirect exact from="/" to="/money"/>
-          <Route path="*">
-            <NoMatch/>
-          </Route>
-        </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from="/" to="/money"/>
+                <Route path="*">
+                    <NoMatch/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
-function NoMatch() {
-  return (
-      <div>页面不存在，你丫输错地址了吧！</div>
-  );
-}
-
-function Statistics() {
-  return (
-      <Layout>
-        <h2>统计页面</h2>
-      </Layout>
-  );
-}
-
-function Tags() {
-  return (
-      <Layout>
-        <h2>标签页面</h2>
-      </Layout>
-  );
-}
-
-function Money() {
-  return (
-      <Layout>
-        <h2>记账页面</h2>
-      </Layout>
-  );
-}
 
 export default App;
