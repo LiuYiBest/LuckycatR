4a9099184e10e1545c647effb1699390d8a14831
chunfen <1070692044@qq.com>
2021-09-10 19:35:00 +0800

    exact精准路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import Money from './views/Money';
@@ -6,6 +5,7 @@ import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
 import styled from 'styled-components';
+import {Tag} from './views/Tag';
 
 const AppWrapper = styled.div`
   color: #333;
@@ -16,13 +16,16 @@ function App() {
         <AppWrapper>
             <Router>
                 <Switch>
-                    <Route path="/tags">
+                    <Route exact path="/tags">
                         <Tags/>
                     </Route>
-                    <Route path="/money">
+                    <Route exact path="/tags/:tag">
+                        <Tag/>
+                    </Route>
+                    <Route exact path="/money">
                         <Money/>
                     </Route>
-                    <Route path="/statistics">
+                    <Route exact path="/statistics">
                         <Statistics/>
                     </Route>
                     <Redirect exact from="/" to="/money"/>
